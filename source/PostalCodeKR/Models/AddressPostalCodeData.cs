0a13734e832b03f853ea6ac48681124e3de202a5
URK96 <chlwlsgur96@hotmail.com>
2025-04-01 01:55:57 +0900

    Add summary comment for API doc creation

--- source/PostalCodeKR/Models/AddressPostalCodeData.cs
+++ source/PostalCodeKR/Models/AddressPostalCodeData.cs
@@ -2,9 +2,18 @@ namespace PostalCodeKR.Models;
 
 public record AddressPostalCodeData
 {
+    /// <summary>
+    /// Postal code (Zip code) of target address (우편 번호)
+    /// </summary>
     public string PostalCode { get; init; }
 
+    /// <summary>
+    /// Road number address (도로명 주소)
+    /// </summary>
     public string RoadNumberAddress { get; init; }
 
+    /// <summary>
+    /// Lot number address (지번 주소)
+    /// </summary>
     public string LotNumberAddress { get; init; }
 }
--- source/PostalCodeKR/Models/ServiceResponseData.cs
+++ source/PostalCodeKR/Models/ServiceResponseData.cs
@@ -1,16 +1,38 @@
 namespace PostalCodeKR.Models;
 
+/// <summary>
+/// Response data class about API service request
+/// </summary>
 public record ServiceResponseData
 {
+
+    /// <summary>
+    /// Success information of API service request process
+    /// </summary>
     public bool ServiceRequestSuccess { get; init; }
 
+    /// <summary>
+    /// All page total item count of search result
+    /// </summary>
     public int TotalItemCount { get; init; }
 
+    /// <summary>
+    /// Requested item count per page
+    /// </summary>
     public int ItemCountPerPage { get; init; }
 
+    /// <summary>
+    /// Total page count
+    /// </summary>
     public int TotalPageCount { get; init; }
 
+    /// <summary>
+    /// Current page number
+    /// </summary>
     public int CurrentPage { get; init; }
 
+    /// <summary>
+    /// Search result list (read-only)
+    /// </summary>
     public IReadOnlyList<AddressPostalCodeData> SearchResults { get; init; }
 }
